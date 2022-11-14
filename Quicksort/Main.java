5bfe42c443ad97aa0c8e50668770ec2e9ce2d03d
suzzingv <84590065+suzzingv@users.noreply.github.com>
2022-11-15 01:41:26 +0900

    b

--- Quicksort/Main.java
+++ Quicksort/Main.java
@@ -24,20 +24,20 @@ public class Main {
     public static void quick_sort(int[] arr, int start, int end) {
         int left = start;
         int right = end;
-        int pivot = (start + end) / 2;
+        int pivot = arr[(start + end) / 2];
 
         if (start >= end)
             return;
-        System.out.println("pass");
+
         while (left <= right) { //left와 right가 뒤바뀌면 멈춤 //pivot에서 뒤바뀔 수 밖에 없음
-            while (left != pivot && arr[left] < arr[pivot]) //조건에 안 맞을 때까지 left 이동하기
+            while (arr[left] < pivot) //조건에 안 맞을 때까지 left 이동하기
                 left++;
-            while (right != pivot && arr[right] > arr[pivot]) //조건에 안 맞을 때까지 right 이동하기
+            while (arr[right] > pivot) //조건에 안 맞을 때까지 right 이동하기
                 right--;
-            System.out.println("left: " + left + " " + "right : " + right);
-            compare_RL(arr, left, right);
-            System.out.println("swap : " + arr[left] + " " + arr[right]);
-            left ++; right --;
+            if (left <= right) {
+                compare_RL(arr, left, right);
+                left ++; right --;
+            }
         }
 
         quick_sort(arr, start, right);
@@ -47,10 +47,8 @@ public class Main {
     public static void compare_RL(int[] arr, int left, int right) {
         int tmp;
 
-        if (arr[left] > arr[right]) {
-            tmp = arr[left];
-            arr[left] = arr[right];
-            arr[right] = tmp;
-        }
+        tmp = arr[left];
+        arr[left] = arr[right];
+        arr[right] = tmp;
     }
 }
