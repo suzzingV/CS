51d9ac2635b44d67d837a30e3eaa50c51cc17abf
suzzingv <84590065+suzzingv@users.noreply.github.com>
2023-02-22 23:56:11 +0900

    하노이탑

---
+++ chapter2/sec02/HanoiTowerSolu.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void HanoiTowerMove(int num, char from, char by, char to) {
+    if(num == 1)
+    {
+        printf("원반 1을 %c에서 %c로 이동 \n", from, to);
+        return;
+    }
+    
+    HanoiTowerMove(num - 1, from, to, by);
+    printf("원반 %d을(를) %c에서 %c로 이동 \n", num, from, to);
+    HanoiTowerMove(num - 1, by, from, to);
+}
+int main()
+{
+    HanoiTowerMove(3, 'A', 'B', 'C');
+    return 0;
+}
