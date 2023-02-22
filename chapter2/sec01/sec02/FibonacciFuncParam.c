d191e8ea2bf2988f1a0283d9511889667af21ca0
suzzingv <84590065+suzzingv@users.noreply.github.com>
2023-02-22 23:08:59 +0900

    피보나치 함수 실행 순서

---
+++ chapter2/sec01/sec02/FibonacciFuncParam.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int Fibo(int n)
+{
+    printf("func call param %d \n", n);
+    if(n == 1) 
+        return 0;
+    if(n == 2)
+        return 1;
+    return Fibo(n - 1) + Fibo(n - 2);
+}
+
+int main() 
+{
+    Fibo(7);
+    return 0;
+}
