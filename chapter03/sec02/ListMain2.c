021bff7de467f14c55d65e5393c76f371ee7a885
sjlim1999@naver.com <sjlim1999@naver.com>
2023-04-24 21:51:03 +0900

    미완성

---
+++ chapter03/sec02/ListMain2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "ArrayList.h"
+
+int main()
+{
+    List list;
+    int data;
+    ListInit(&list);
+
+    for(int i = 1; i < 10; i++)
+    {
+        LInsert(&list, i);
+    }
+    
+    if()
+}
