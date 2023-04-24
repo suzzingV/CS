31f7760e7334b66093bd86d85616a79abbed80b3
sjlim1999@naver.com <sjlim1999@naver.com>
2023-04-24 22:44:52 +0900

    문제 3-1

--- chapter03/sec02/ListMain2.c
+++ chapter03/sec02/ListMain2.c
@@ -12,5 +12,32 @@ int main()
         LInsert(&list, i);
     }
     
-    if()
+    int sum = 0;
+    if(LFirst(&list, &data))
+    {
+        sum += data;
+        while(LNext(&list, &data)) 
+        {
+            sum += data;
+        }
+    }
+    printf("데이터의 합: %d \n", sum);
+
+    if(LFirst(&list, &data)) {
+        if(data % 2 == 0 || data % 3 == 0) 
+            LRemove(&list);
+        while(LNext(&list, &data))
+        {
+            if(data % 2 == 0 || data % 3 == 0) 
+                LRemove(&list);
+        }
+    }
+
+    if(LFirst(&list, &data))
+    {
+        printf("%d ", data);
+        while(LNext(&list, &data))
+            printf("%d ", data);
+    }
+    return 0;
 }
