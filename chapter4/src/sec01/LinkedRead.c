0a571ffcb7b62232586ad00147b30366ca679e4e
suzzingv <84590065+suzzingv@users.noreply.github.com>
2023-05-02 00:45:09 +0900

    연결리스트 데이터 입력, 조회, 삭제

---
+++ chapter4/src/sec01/LinkedRead.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct _node
+{
+    int data;
+    struct _node* next;
+} Node;
+
+int main(void) 
+{
+    Node* head = NULL;
+    Node* cur = NULL;
+    Node* tail = NULL;
+
+    Node* newNode = NULL;
+    int readData;
+
+    //데이터 읽기
+    while(1)
+    {
+        printf("자연수 읽기: ");
+        scanf("%d", &readData);
+        if(readData < 1)
+            break;
+        
+        newNode = (Node*) malloc(sizeof(Node));
+        newNode -> data = readData;
+        newNode -> next = NULL;
+
+        if(head == NULL)
+            head = newNode;
+        else
+            tail -> next = newNode;
+        
+        tail = newNode;
+    }
+    printf("\n");
+
+    //데이터 조회
+    printf("입력받은 데이터의 전체 출력! \n");
+    if(head == NULL)
+        printf("저장된 자연수가 존재하지 않습니다. \n");
+    else 
+    {
+        cur = head;
+        printf("%d ", cur -> data);
+        
+        while(cur -> next != NULL)
+        {
+            cur = cur -> next;
+            printf("%d ", cur -> data);
+        }
+    }
+    printf("\n\n");
+
+    //메모리 해제
+    if(head == NULL)
+        return 0;
+    else
+    {
+        Node* delNode = head;
+        Node* delNextNode = head -> next;
+
+        printf("%d을(를) 삭제합니다. \n", delNode -> data);
+        free(delNode);
+
+        while(delNextNode != NULL)
+        {
+            delNode = delNextNode;
+            delNextNode = delNode -> next;
+
+            printf("%d을(를) 삭제합니다. \n", delNode -> data);
+            free(delNode);
+        }
+    }
+    return 0;
+}
